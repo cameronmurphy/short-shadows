3907596a001b9ea710ae045123bee0afd1fc1a2d
Cameron Murphy <camurphy@gmail.com>
2019-03-05 12:59:04 +1100

    WIP gigs-block

--- templates/_scripts/main.js
+++ templates/_scripts/main.js
@@ -4,4 +4,5 @@ import '../_components/header';
 import '../_components/mobile-menu';
 
 import '../_components/album-block';
+import '../_components/gigs-block';
 import '../_components/heading-block';
