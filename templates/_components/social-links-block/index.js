4f754e37b380fe76323c7bdd3edfa295c5ee59d2
Cameron Murphy <camurphy@gmail.com>
2019-03-09 15:36:10 +1100

    Tree shake the necessary icons

--- templates/_components/social-links-block/index.js
+++ templates/_components/social-links-block/index.js
@@ -1,1 +1,6 @@
+import { dom, library } from '@fortawesome/fontawesome-svg-core'; // eslint-disable-line no-unused-vars
+import { faFacebook, faInstagram } from '@fortawesome/free-brands-svg-icons'; // eslint-disable-line no-unused-vars
 import './styles.scss';
+
+library.add(faFacebook, faInstagram);
+dom.watch();
