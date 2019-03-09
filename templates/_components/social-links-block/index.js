388b5cbc8dadfc7f936c87048fed1ded017b04ab
Cameron Murphy <camurphy@gmail.com>
2019-03-09 15:56:43 +1100

    Remove eslint hints

--- templates/_components/social-links-block/index.js
+++ templates/_components/social-links-block/index.js
@@ -1,5 +1,5 @@
-import { dom, library } from '@fortawesome/fontawesome-svg-core'; // eslint-disable-line no-unused-vars
-import { faFacebook, faInstagram } from '@fortawesome/free-brands-svg-icons'; // eslint-disable-line no-unused-vars
+import { dom, library } from '@fortawesome/fontawesome-svg-core';
+import { faFacebook, faInstagram } from '@fortawesome/free-brands-svg-icons';
 import './styles.scss';
 
 library.add(faFacebook, faInstagram);
