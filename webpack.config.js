ffd235ce797eb6742cc8e7ad3c4cf1811b3ccc83
Cameron Murphy <camurphy@gmail.com>
2019-08-17 16:17:55 +1000

    Disable resolveUrlLoader again, format webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -5,8 +5,7 @@ if (!Encore.isRuntimeEnvironmentConfigured()) {
   Encore.configureRuntimeEnvironment(process.env.NODE_ENV || 'dev');
 }
 
-Encore
-  .setOutputPath('web/static/assets')
+Encore.setOutputPath('web/static/assets')
   .setPublicPath('/static/assets')
   .addEntry('main', './templates/_scripts/main.js')
   .copyFiles({
@@ -18,13 +17,15 @@ Encore
   .cleanupOutputBeforeBuild()
   .enableSourceMaps(!Encore.isProduction())
   .enableVersioning()
-  .enableSassLoader()
+  .enableSassLoader(() => {}, {
+    // https://github.com/symfony/webpack-encore/issues/253
+    resolveUrlLoader: false,
+  })
   .enablePostCssLoader()
   .configureBabel(() => {}, {
     useBuiltIns: 'usage',
-    corejs: 3
-  })
-;
+    corejs: 3,
+  });
 
 if (Encore.isProduction()) {
   Encore.addPlugin(
