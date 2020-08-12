a3af1ed0ded90033ad91b542c2cb5c495caa65e3
Cameron Murphy <camurphy@gmail.com>
2020-08-12 22:28:51 +1000

    Add text-block

--- templates/_scripts/main.js
+++ templates/_scripts/main.js
@@ -3,11 +3,12 @@ import '../_styles/main.scss';
 import '../_components/header';
 import '../_components/mobile-menu';
 
-import '../_components/dual-image-block';
-import '../_components/release-block';
 import '../_components/contact-block';
+import '../_components/dual-image-block';
 import '../_components/gallery-block';
 import '../_components/gigs-block';
 import '../_components/heading-block';
+import '../_components/release-block';
 import '../_components/social-links-block';
+import '../_components/text-block';
 import '../_components/youtube-block';
