b1afaed207106ed4a4c268f6f441daff3a05d7f9
Cameron Murphy <camurphy@gmail.com>
2020-08-12 21:19:28 +1000

    Add YouTube to social-link-block

--- templates/_components/social-links-block/index.js
+++ templates/_components/social-links-block/index.js
@@ -1,6 +1,6 @@
 import { dom, library } from '@fortawesome/fontawesome-svg-core';
-import { faFacebook, faInstagram } from '@fortawesome/free-brands-svg-icons';
+import { faFacebook, faInstagram, faYoutube } from '@fortawesome/free-brands-svg-icons';
 import './styles.scss';
 
-library.add(faFacebook, faInstagram);
+library.add(faFacebook, faInstagram, faYoutube);
 dom.watch();
