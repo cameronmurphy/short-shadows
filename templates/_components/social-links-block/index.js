f7ebd0a1d6bee052d9861b915bf94f737d396e41
Cameron Murphy <camurphy@gmail.com>
2020-08-12 21:38:21 +1000

    Add SoundCloud to social-media-block

--- templates/_components/social-links-block/index.js
+++ templates/_components/social-links-block/index.js
@@ -1,6 +1,6 @@
 import { dom, library } from '@fortawesome/fontawesome-svg-core';
-import { faFacebook, faInstagram, faYoutube } from '@fortawesome/free-brands-svg-icons';
+import { faFacebook, faInstagram, faSoundcloud, faYoutube } from '@fortawesome/free-brands-svg-icons';
 import './styles.scss';
 
-library.add(faFacebook, faInstagram, faYoutube);
+library.add(faFacebook, faInstagram, faSoundcloud, faYoutube);
 dom.watch();
